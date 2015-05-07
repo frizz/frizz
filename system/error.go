e1c301a5f1e4a34efd96052c0901b6b7c2630156
David Brophy <dave@brophy.uk>
2015-05-07 08:06:09 +0200

    Err and Error

---
+++ system/error.go
@@ -0,0 +1,30 @@
+package system
+
+import "fmt"
+
+// Set RETURN_ERRORS to true to bubble up plain errors without extra formatting
+var RETURN_ERRORS = false
+
+func Err(inner error, location string, descriptionFormat string, descriptionArgs ...interface{}) error {
+	if inner != nil && RETURN_ERRORS {
+		return inner
+	}
+	return Error{
+		Error:       inner,
+		Location:    location,
+		Description: fmt.Sprintf(descriptionFormat, descriptionArgs...),
+	}
+}
+
+type Error struct {
+	Error       error
+	Location    string
+	Description string
+}
+
+func (e Error) Error() string {
+	if e.Error == nil {
+		return fmt.Sprintf("Error in %s: %s.", e.Location, e.Description)
+	}
+	return fmt.Sprintf("Error in %s: %s returned an error: \n%v\n", e.Location, e.Description, e.Error)
+}
