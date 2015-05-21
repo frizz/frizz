85500c4ceaf45c026c8937484bfaf2e2b8182275
David Brophy <dave@brophy.uk>
2015-05-21 12:45:46 +0100

    Demo enforcers and tests

--- system/enforcer.go
+++ system/enforcer.go
@@ -12,9 +12,9 @@ type Enforcer interface {
 }
 
 func (r *String_rule) Enforce(data interface{}, path string, imports map[string]string) (bool, string, error) {
-	s, ok := data.(*String)
+	s, ok := data.(String)
 	if !ok {
-		return false, "", uerr.New("SXFBXGQSEA", nil, "String_rule.Enforce", "data %T should be *String.", data)
+		return false, "", uerr.New("SXFBXGQSEA", nil, "String_rule.Enforce", "data %T should be system.String.", data)
 	}
 	if r.Equal.Exists {
 		if !s.Exists {
@@ -24,11 +24,11 @@ func (r *String_rule) Enforce(data interface{}, path string, imports map[string]
 			return false, fmt.Sprintf("Equal: Value must equal '%s'", r.Equal.Value), nil
 		}
 	}
-	//if r.MaxLength.Exists {
-	//	if s.Exists && len(s.Value) > r.MaxLength.Value {
-	//		return false, fmt.Sprintf("MaxLength: Length must not be greater than %d", r.MaxLength.Value), nil
-	//	}
-	//}
+	if r.MaxLength.Exists {
+		if s.Exists && len(s.Value) > r.MaxLength.Value {
+			return false, fmt.Sprintf("MaxLength: Length must not be greater than %d", r.MaxLength.Value), nil
+		}
+	}
 	// TODO: Implement rules
 	return true, "", nil
 }
---
+++ system/enforcer_test.go
@@ -0,0 +1,42 @@
+package system
+
+import (
+	"testing"
+
+	"kego.io/uerr"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringRule_Enforce(t *testing.T) {
+	r := String_rule{Equal: NewString("a"), MaxLength: NewInt(1)}
+	ok, message, err := r.Enforce(NewString("a"), "", map[string]string{})
+	assert.NoError(t, err)
+	assert.Equal(t, "", message)
+	assert.True(t, ok)
+
+	ok, message, err = r.Enforce(String{}, "", map[string]string{})
+	assert.NoError(t, err)
+	assert.Equal(t, "Equal: Value must exist, and equal 'a'", message)
+	assert.False(t, ok)
+
+	ok, message, err = r.Enforce(NewString("b"), "", map[string]string{})
+	assert.NoError(t, err)
+	assert.Equal(t, "Equal: Value must equal 'a'", message)
+	assert.False(t, ok)
+
+	ok, message, err = r.Enforce("a", "", map[string]string{})
+	uerr.Assert(t, err, "SXFBXGQSEA")
+
+	r = String_rule{MaxLength: NewInt(1)}
+	ok, message, err = r.Enforce(NewString("ab"), "", map[string]string{})
+	assert.NoError(t, err)
+	assert.Equal(t, "MaxLength: Length must not be greater than 1", message)
+	assert.False(t, ok)
+
+	ok, message, err = r.Enforce(String{}, "", map[string]string{})
+	assert.NoError(t, err)
+	assert.Equal(t, "", message)
+	assert.True(t, ok)
+
+}
