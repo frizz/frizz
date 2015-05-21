2f15d0a32e29424dd82cf09da1835b3a578e84f8
David Brophy <dave@brophy.uk>
2015-05-21 12:58:12 +0100

    Demo validators and tests

--- system/validator.go
+++ system/validator.go
@@ -1,6 +1,17 @@
 package system
 
+import "fmt"
+
 // Validator is a type that needs to have it's data validated.
 type Validator interface {
 	Validate(path string, imports map[string]string) (bool, string, error)
+}
+
+func (r *String_rule) Validate(path string, imports map[string]string) (bool, string, error) {
+	if r.MaxLength.Exists && r.MinLength.Exists {
+		if r.MaxLength.Value < r.MinLength.Value {
+			return false, fmt.Sprintf("MaxLength %d must not be lass than MinLength %d", r.MaxLength.Value, r.MinLength.Value), nil
+		}
+	}
+	return true, "", nil
 }
---
+++ system/validator_test.go
@@ -0,0 +1,39 @@
+package system
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringRule_Validate(t *testing.T) {
+	r := &String_rule{}
+	ok, message, err := r.Validate("", map[string]string{})
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, "", message)
+
+	r = &String_rule{MaxLength: NewInt(10)}
+	ok, message, err = r.Validate("", map[string]string{})
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, "", message)
+
+	r = &String_rule{MaxLength: NewInt(10), MinLength: NewInt(5)}
+	ok, message, err = r.Validate("", map[string]string{})
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, "", message)
+
+	r = &String_rule{MaxLength: NewInt(5), MinLength: NewInt(5)}
+	ok, message, err = r.Validate("", map[string]string{})
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, "", message)
+
+	r = &String_rule{MaxLength: NewInt(4), MinLength: NewInt(5)}
+	ok, message, err = r.Validate("", map[string]string{})
+	assert.NoError(t, err)
+	assert.False(t, ok)
+	assert.Equal(t, "MaxLength 4 must not be lass than MinLength 5", message)
+}
