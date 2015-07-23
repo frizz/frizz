b166f4badbd7cef1a4ac317e6d6805ae3d1229af
David Brophy <dave@brophy.uk>
2015-07-23 15:22:25 -0400

    Fixed kego_test
    Former-commit-id: 339600a428eaf0f891c16ce36f019e3280680302

--- kego_test.go
+++ kego_test.go
@@ -1,7 +1,13 @@
-package kego
+package kego_test
 
-import "testing"
+import (
+	"testing"
+
+	"kego.io"
+	"kego.io/kerr/assert"
+)
 
 func TestKego(t *testing.T) {
-
+	_, err := kego.Open("", "", map[string]string{})
+	assert.NoError(t, err)
 }
