ad6be7ffc7127e9bf9fd97182596436ede6547db
David Brophy <dave@brophy.uk>
2015-10-17 20:42:10 -0500

    Removed reflection utilities
    Former-commit-id: 0cbfbe4bd11fb4cce7ae732c05c8c58a143f8f77

--- system/rule_test.go
+++ system/rule_test.go
@@ -106,82 +106,22 @@ func TestRuleWrapperItemsRule(t *testing.T) {
 	_, err = w.ItemsRule()
 	assert.IsError(t, err, "VPAGXSTQHM")
 
-	parentType.Native = NewString("array")
 	/*
-		rh.Rule = "a"
-		_, err = rh.ItemsRule()
-		// rh.rule must be a pointer or RuleFieldByReflection will error
-		assert.IsError(t, err, "LIDXIQYGJD")
+		parentType.Native = NewString("array")
 
-		rh.Rule = &struct{}{}
-		_, err = rh.ItemsRule()
-		// rh.rule needs an Items field
-		assert.IsError(t, err, "VYTHGJTSNJ")
+			rh.Rule = "a"
+			_, err = rh.ItemsRule()
+			// rh.rule must be a pointer or RuleFieldByReflection will error
+			assert.IsError(t, err, "LIDXIQYGJD")
 
-		rh.Rule = &struct{ Items int }{Items: 1}
-		_, err = rh.ItemsRule()
-		// Items must be a rule or NewRuleHolder will error
-		assert.IsError(t, err, "FGYMQPNBQJ")
+			rh.Rule = &struct{}{}
+			_, err = rh.ItemsRule()
+			// rh.rule needs an Items field
+			assert.IsError(t, err, "VYTHGJTSNJ")
+
+			rh.Rule = &struct{ Items int }{Items: 1}
+			_, err = rh.ItemsRule()
+			// Items must be a rule or NewRuleHolder will error
+			assert.IsError(t, err, "FGYMQPNBQJ")
 	*/
-}
-func TestRuleFieldByReflection(t *testing.T) {
-
-	_, _, _, err := RuleFieldByReflection(struct{}{}, "")
-	// Must be a pointer type
-	assert.IsError(t, err, "QOYMWPXWUO")
-
-	i := 1
-	_, _, _, err = RuleFieldByReflection(&i, "")
-	// Must point to a struct type
-	assert.IsError(t, err, "IGOUOBGXAN")
-
-	_, _, ok, err := RuleFieldByReflection(&struct{}{}, "A")
-	// Must have field called a, or it returns false
-	assert.NoError(t, err)
-	assert.False(t, ok)
-
-	_, _, ok, err = RuleFieldByReflection(&struct{ A *string }{A: nil}, "A")
-	// If field is a pointer, and nil, returns false
-	assert.NoError(t, err)
-	assert.False(t, ok)
-
-	_, _, ok, err = RuleFieldByReflection(&struct{ A string }{A: ""}, "A")
-	// If field is not a pointer, and equal to the zero value for that type, returns false
-	assert.NoError(t, err)
-	assert.False(t, ok)
-
-	_, _, ok, err = RuleFieldByReflection(&struct {
-		A struct {
-			B int
-			C string
-		}
-	}{A: struct {
-		B int
-		C string
-	}{B: 0, C: ""}}, "A")
-	// If field is not a pointer, and equal to the zero value for that type, returns false
-	assert.NoError(t, err)
-	assert.False(t, ok)
-
-	fi, fpi, ok, err := RuleFieldByReflection(&struct{ A string }{A: "b"}, "A")
-	assert.NoError(t, err)
-	assert.True(t, ok)
-	f, ok := fi.(string)
-	assert.True(t, ok)
-	assert.Equal(t, "b", f)
-	fp, ok := fpi.(*string)
-	assert.True(t, ok)
-	assert.Equal(t, "b", *fp)
-
-	b := "b"
-	fi, fpi, ok, err = RuleFieldByReflection(&struct{ A *string }{A: &b}, "A")
-	assert.NoError(t, err)
-	assert.True(t, ok)
-	fp, ok = fi.(*string)
-	assert.True(t, ok)
-	assert.Equal(t, "b", *fp)
-	fp, ok = fpi.(*string)
-	assert.True(t, ok)
-	assert.Equal(t, "b", *fp)
-
 }
