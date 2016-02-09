5fa8d9b1e150002a167ce57f6e22ff9f79f02e20
David Brophy <dave@brophy.uk>
2016-02-09 08:44:54 +0100

    TestLogger, TestMultiWriter

--- process/run_test.go
+++ process/run_test.go
@@ -10,11 +10,40 @@ import (
 
 	"fmt"
 
+	"bytes"
+
 	"kego.io/context/envctx"
 	"kego.io/kerr"
 	"kego.io/kerr/assert"
 	"kego.io/process/tests"
 )
+
+func TestMultiWriter(t *testing.T) {
+	var p, w1, w2 []byte
+	pb := bytes.NewBuffer(p)
+	w1b := bytes.NewBuffer(w1)
+	w2b := bytes.NewBuffer(w2)
+	mw := MultiWriter(pb, w1b, w2b)
+	mw.Write([]byte("a"))
+	mw.Write([]byte("b"))
+	assert.Equal(t, "ab", pb.String())
+	assert.Equal(t, "ab", w1b.String())
+	assert.Equal(t, "ab", w2b.String())
+}
+
+func TestLogger(t *testing.T) {
+	_, o, e := logger(false)
+	_, ok := o.(*bytes.Buffer)
+	assert.True(t, ok)
+	_, ok = e.(*bytes.Buffer)
+	assert.True(t, ok)
+
+	_, o, e = logger(true)
+	_, ok = o.(*multiWriter)
+	assert.True(t, ok)
+	_, ok = e.(*multiWriter)
+	assert.True(t, ok)
+}
 
 func TestRun(t *testing.T) {
 
