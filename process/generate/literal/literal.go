30626c17b875cac8c45ea97dc24d49b5b62c637f
David Brophy <dave@brophy.uk>
2016-02-25 09:48:31 +0100

    Exclude literal from tests

--- process/generate/literal/literal.go
+++ process/generate/literal/literal.go
@@ -1,1 +1,6 @@
 package literal // import "kego.io/process/generate/literal"
+
+// ke: {"package": {"notest": true}}
+
+// This package is no longer used, but I don't want to delete it just yet. I will exclude from the
+// tests while it's not in use.
