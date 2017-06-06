f1763118c92c5a1d0ad5f06b2d8339ab5a288360
David Brophy <dave@brophy.uk>
2017-06-06 09:55:35 -0400

    Fixed test

--- system/generate/generate_test.go
+++ system/generate/generate_test.go
@@ -3,11 +3,20 @@ package generate
 import (
 	"bytes"
 	"testing"
+
+	"github.com/dave/patsy"
+	"github.com/dave/patsy/vos"
 )
 
 func TestGenerate(t *testing.T) {
 	buf := &bytes.Buffer{}
-	if err := Generate(buf, "frizz.io/site"); err != nil {
+	env := vos.Os()
+	path := "frizz.io/tests/unpacker"
+	dir, err := patsy.Dir(env, path)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if err := Generate(buf, env, path, dir); err != nil {
 		t.Fatal(err.Error())
 	}
 }
