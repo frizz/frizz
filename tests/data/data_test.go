f7d6d6bd7c49d83954cbd588e5d248d651049b43
Dave Brophy <dave@brophy.uk>
2017-07-27 10:05:28 -0500

    Improved tests

--- tests/data/data_test.go
+++ tests/data/data_test.go
@@ -1,17 +1,34 @@
 package data
 
 import (
-	"fmt"
 	"testing"
 
 	"frizz.io/frizz"
+	"frizz.io/tests/packer"
+	"frizz.io/tests/packer/sub"
 )
 
-func TestAddImports(t *testing.T) {
+func TestImports(t *testing.T) {
 	obs, err := frizz.Package(Imports)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("%#v", obs)
-
+	if len(obs) != 1 {
+		t.Fail()
+	}
+	d, ok := obs["data"]
+	if !ok {
+		t.Fail()
+	}
+	o, ok := d.(packer.SubInterface)
+	if !ok {
+		t.Fail()
+	}
+	s, ok := o.SubInterface.(sub.Sub)
+	if !ok {
+		t.Fail()
+	}
+	if s.String != "foo" {
+		t.Fail()
+	}
 }
