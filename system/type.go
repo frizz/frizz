5df398af8d4a5076a4c6ab1aa4e83f76380432df
Dave Brophy <davebrophy@spotify.com>
2017-08-01 14:50:52 -0500

    Added stack to validators

--- system/type.go
+++ system/type.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 
 	"frizz.io/common"
+	"frizz.io/frizz"
 	"github.com/pkg/errors"
 )
 
@@ -12,32 +13,26 @@ type Type struct {
 	Validators []common.Validator
 }
 
-func (t Type) Validate(value interface{}) (valid bool, message string, err error) {
-	return t.validate(reflect.ValueOf(value))
+func (t Type) Validate(stack frizz.Stack, value interface{}) (valid bool, message string, err error) {
+	return t.ValidateValue(stack, reflect.ValueOf(value))
 }
 
-func (t Type) validate(value reflect.Value) (valid bool, message string, err error) {
-
+func (t Type) ValidateValue(stack frizz.Stack, value reflect.Value) (valid bool, message string, err error) {
 	switch value.Kind() {
 	case reflect.Invalid, reflect.Chan, reflect.Func, reflect.UnsafePointer:
-
-		return false, "", errors.Errorf("can't validate kind %s", value.Kind())
-
+		return false, "", errors.Errorf("%s: can't validate kind %s", stack, value.Kind())
 	case reflect.Ptr, reflect.Interface:
-
-		valid, message, err = t.Validate(value.Elem())
+		valid, message, err = t.Validate(stack, value.Elem())
 		if err != nil {
 			return false, "", err
 		}
 		if !valid {
 			return false, message, nil
 		}
-
 	default:
-
 		for _, v := range t.Validators {
 			if vv, ok := v.(common.ValueValidator); ok {
-				valid, message, err = vv.ValidateValue(value)
+				valid, message, err = vv.ValidateValue(stack, value)
 				if err != nil {
 					return false, "", err
 				}
@@ -45,7 +40,7 @@ func (t Type) validate(value reflect.Value) (valid bool, message string, err err
 					return false, message, nil
 				}
 			} else {
-				valid, message, err = v.Validate(value.Interface())
+				valid, message, err = v.Validate(stack, value.Interface())
 				if err != nil {
 					return false, "", err
 				}
@@ -54,84 +49,6 @@ func (t Type) validate(value reflect.Value) (valid bool, message string, err err
 				}
 			}
 		}
-
-		/*
-			case reflect.Array, reflect.Map, reflect.Slice:
-
-				// validate map keys
-				if len(t.Keys) > 0 && value.Kind() == reflect.Map {
-					for i := 0; i < value.Len(); i++ {
-						keyValue := value.MapKeys()[i]
-						for _, validator := range t.Items {
-							valid, message, err = validator.Validate(keyValue.Interface())
-							if err != nil {
-								return false, "", err
-							}
-							if !valid {
-								return false, message, nil
-							}
-						}
-					}
-				}
-
-				// validate collection items
-				if len(t.Items) > 0 {
-					for i := 0; i < value.Len(); i++ {
-						var itemValue reflect.Value
-						if value.Kind() == reflect.Map {
-							itemValue = value.MapIndex(value.MapKeys()[i])
-						} else {
-							itemValue = value.Index(i)
-						}
-						for _, validator := range t.Items {
-							valid, message, err = validator.Validate(itemValue.Interface())
-							if err != nil {
-								return false, "", err
-							}
-							if !valid {
-								return false, message, nil
-							}
-						}
-					}
-				}
-
-			case reflect.Struct:
-
-				if len(t.Fields) > 0 {
-					for i := 0; i < value.Type().NumField(); i++ {
-						f := value.Type().Field(i)
-						validators, ok := t.Fields[f.Name]
-						if !ok || len(validators) == 0 {
-							continue
-						}
-						fieldValue := value.Field(i)
-						for _, validator := range validators {
-							valid, message, err = validator.Validate(fieldValue.Interface())
-							if err != nil {
-								return false, "", err
-							}
-							if !valid {
-								return false, message, nil
-							}
-						}
-					}
-				}
-
-			default:
-
-				// native value
-				if len(t.Value) > 0 {
-					for _, validator := range t.Value {
-						valid, message, err = validator.Validate(value.Interface())
-						if err != nil {
-							return false, "", err
-						}
-						if !valid {
-							return false, message, nil
-						}
-					}
-				}
-		*/
 	}
 	return true, "", nil
 }
--- validators/numbers.go
+++ validators/numbers.go
@@ -7,6 +7,7 @@ import (
 
 	"reflect"
 
+	"frizz.io/frizz"
 	"frizz.io/system"
 	"github.com/pkg/errors"
 )
@@ -14,160 +15,160 @@ import (
 // frizz
 type GreaterThan json.Number
 
-func (g GreaterThan) Validate(input interface{}) (valid bool, message string, err error) {
-	return g.ValidateValue(reflect.ValueOf(input))
+func (g GreaterThan) Validate(stack frizz.Stack, input interface{}) (valid bool, message string, err error) {
+	return g.ValidateValue(stack, reflect.ValueOf(input))
 }
 
-func (g GreaterThan) ValidateValue(value reflect.Value) (valid bool, message string, err error) {
-	return compare(value, ">", json.Number(g))
+func (g GreaterThan) ValidateValue(stack frizz.Stack, value reflect.Value) (valid bool, message string, err error) {
+	return compare(stack, value, ">", json.Number(g))
 }
 
 // frizz
 type LessThan json.Number
 
-func (l LessThan) Validate(input interface{}) (valid bool, message string, err error) {
-	return l.ValidateValue(reflect.ValueOf(input))
+func (l LessThan) Validate(stack frizz.Stack, input interface{}) (valid bool, message string, err error) {
+	return l.ValidateValue(stack, reflect.ValueOf(input))
 }
 
-func (l LessThan) ValidateValue(value reflect.Value) (valid bool, message string, err error) {
-	return compare(value, "<", json.Number(l))
+func (l LessThan) ValidateValue(stack frizz.Stack, value reflect.Value) (valid bool, message string, err error) {
+	return compare(stack, value, "<", json.Number(l))
 }
 
 // frizz
 type GreaterThanOrEqual json.Number
 
-func (g GreaterThanOrEqual) Validate(input interface{}) (valid bool, message string, err error) {
-	return g.ValidateValue(reflect.ValueOf(input))
+func (g GreaterThanOrEqual) Validate(stack frizz.Stack, input interface{}) (valid bool, message string, err error) {
+	return g.ValidateValue(stack, reflect.ValueOf(input))
 }
 
-func (g GreaterThanOrEqual) ValidateValue(value reflect.Value) (valid bool, message string, err error) {
-	return compare(value, ">=", json.Number(g))
+func (g GreaterThanOrEqual) ValidateValue(stack frizz.Stack, value reflect.Value) (valid bool, message string, err error) {
+	return compare(stack, value, ">=", json.Number(g))
 }
 
 // frizz
 type LessThanOrEqual json.Number
 
-func (l LessThanOrEqual) Validate(input interface{}) (valid bool, message string, err error) {
-	return l.ValidateValue(reflect.ValueOf(input))
+func (l LessThanOrEqual) Validate(stack frizz.Stack, input interface{}) (valid bool, message string, err error) {
+	return l.ValidateValue(stack, reflect.ValueOf(input))
 }
 
-func (l LessThanOrEqual) ValidateValue(value reflect.Value) (valid bool, message string, err error) {
-	return compare(value, "<=", json.Number(l))
+func (l LessThanOrEqual) ValidateValue(stack frizz.Stack, value reflect.Value) (valid bool, message string, err error) {
+	return compare(stack, value, "<=", json.Number(l))
 }
 
 // frizz
 type Equal system.Raw
 
-func (e Equal) Validate(input interface{}) (valid bool, message string, err error) {
-	return e.ValidateValue(reflect.ValueOf(input))
+func (e Equal) Validate(stack frizz.Stack, input interface{}) (valid bool, message string, err error) {
+	return e.ValidateValue(stack, reflect.ValueOf(input))
 }
 
-func (e Equal) ValidateValue(value reflect.Value) (valid bool, message string, err error) {
+func (e Equal) ValidateValue(stack frizz.Stack, value reflect.Value) (valid bool, message string, err error) {
 	comparison := e.Value
 	switch comparison := comparison.(type) {
 	case json.Number:
-		return compare(value, "==", comparison)
+		return compare(stack, value, "==", comparison)
 	case string:
 		if value.Type().Kind() != reflect.String {
-			return false, "", errors.Errorf("validators.Equal can only accept numeric or string types. Found %T", value.Interface())
+			return false, "", errors.Errorf("%s: validators.Equal can only accept numeric or string types. Found %T", stack, value.Interface())
 		}
 		input := value.Convert(stringtype).Interface().(string)
 		if input != comparison {
-			return false, fmt.Sprintf("value %#v must be equal to %#v", input, comparison), nil
+			return false, fmt.Sprintf("%s: value %#v must be equal to %#v", stack, input, comparison), nil
 		}
 		return true, "", nil
 	}
-	return false, "", errors.Errorf("unsupported type %T for validatros.Equal value", comparison)
+	return false, "", errors.Errorf("%s: unsupported type %T for validatros.Equal value", stack, comparison)
 }
 
-func compare(value reflect.Value, operator string, comparison json.Number) (valid bool, message string, err error) {
+func compare(stack frizz.Stack, value reflect.Value, operator string, comparison json.Number) (valid bool, message string, err error) {
 	switch value.Type().Kind() {
 	case reflect.Interface, reflect.Ptr:
 		// interface or ptr: recurse with elem
-		return compare(value.Elem(), operator, comparison)
+		return compare(stack, value.Elem(), operator, comparison)
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		input := value.Convert(int64type).Interface().(int64)
 		i, err := strconv.ParseInt(string(comparison), 10, 64)
 		if err != nil {
-			return false, "", errors.Wrapf(err, "type %T can only be compared with int64, not %s", value.Interface(), comparison)
+			return false, "", errors.Wrapf(err, "%s: type %T can only be compared with int64, not %s", stack, value.Interface(), comparison)
 		}
 		if operator == ">" {
 			if input <= i {
-				return false, fmt.Sprintf("value %v must be greater than %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be greater than %v", stack, input, i), nil
 			}
 		} else if operator == "<" {
 			if input >= i {
-				return false, fmt.Sprintf("value %v must be less than %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be less than %v", stack, input, i), nil
 			}
 		} else if operator == ">=" {
 			if input < i {
-				return false, fmt.Sprintf("value %v must be greater than or equal to %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be greater than or equal to %v", stack, input, i), nil
 			}
 		} else if operator == "<=" {
 			if input > i {
-				return false, fmt.Sprintf("value %v must be less than or equal to %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be less than or equal to %v", stack, input, i), nil
 			}
 		} else if operator == "==" {
 			if input != i {
-				return false, fmt.Sprintf("value %v must be equal to %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be equal to %v", stack, input, i), nil
 			}
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		input := value.Convert(uint64type).Interface().(uint64)
 		i, err := strconv.ParseUint(string(comparison), 10, 64)
 		if err != nil {
-			return false, "", errors.Wrapf(err, "type %T can only be compared with uint64, not %s", value.Interface(), comparison)
+			return false, "", errors.Wrapf(err, "%s: type %T can only be compared with uint64, not %s", stack, value.Interface(), comparison)
 		}
 		if operator == ">" {
 			if input <= i {
-				return false, fmt.Sprintf("value %v must be greater than %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be greater than %v", stack, input, i), nil
 			}
 		} else if operator == "<" {
 			if input >= i {
-				return false, fmt.Sprintf("value %v must be less than %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be less than %v", stack, input, i), nil
 			}
 		} else if operator == ">=" {
 			if input < i {
-				return false, fmt.Sprintf("value %v must be greater than or equal to %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be greater than or equal to %v", stack, input, i), nil
 			}
 		} else if operator == "<=" {
 			if input > i {
-				return false, fmt.Sprintf("value %v must be less than or equal to %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be less than or equal to %v", stack, input, i), nil
 			}
 		} else if operator == "==" {
 			if input != i {
-				return false, fmt.Sprintf("value %v must be equal to %v", input, i), nil
+				return false, fmt.Sprintf("%s: value %v must be equal to %v", stack, input, i), nil
 			}
 		}
 	case reflect.Float32, reflect.Float64:
 		input := value.Convert(float64type).Interface().(float64)
 		f, err := strconv.ParseFloat(string(comparison), 64)
 		if err != nil {
-			return false, "", errors.Wrapf(err, "type %T can only be compared with float64, not %s", value.Interface(), comparison)
+			return false, "", errors.Wrapf(err, "%s: type %T can only be compared with float64, not %s", stack, value.Interface(), comparison)
 		}
 		if operator == ">" {
 			if input <= f {
-				return false, fmt.Sprintf("value %v must be greater than %v", input, f), nil
+				return false, fmt.Sprintf("%s: value %v must be greater than %v", stack, input, f), nil
 			}
 		} else if operator == "<" {
 			if input >= f {
-				return false, fmt.Sprintf("value %v must be less than %v", input, f), nil
+				return false, fmt.Sprintf("%s: value %v must be less than %v", stack, input, f), nil
 			}
 		} else if operator == ">=" {
 			if input < f {
-				return false, fmt.Sprintf("value %v must be greater than or equal to %v", input, f), nil
+				return false, fmt.Sprintf("%s: value %v must be greater than or equal to %v", stack, input, f), nil
 			}
 		} else if operator == "<=" {
 			if input > f {
-				return false, fmt.Sprintf("value %v must be less than or equal to %v", input, f), nil
+				return false, fmt.Sprintf("%s: value %v must be less than or equal to %v", stack, input, f), nil
 			}
 		} else if operator == "==" {
 			if input != f {
-				return false, fmt.Sprintf("value %v must be equal to %v", input, f), nil
+				return false, fmt.Sprintf("%s: value %v must be equal to %v", stack, input, f), nil
 			}
 		}
 	default:
-		return false, "", errors.Errorf("unknown type for numeric comparison %T", value.Interface())
+		return false, "", errors.Errorf("%s: unknown type for numeric comparison %T", stack, value.Interface())
 	}
 	return true, "", nil
 }
