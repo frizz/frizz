04d5d4108cac3c9d7e0aba49516e916af6d59179
Dave Brophy <davebrophy@spotify.com>
2017-12-19 22:00:12 -0700

    Move regenerate command

--- tests/regenerate/cmd/regenerate/main.go
+++ tests/regenerate/cmd/regenerate/main.go
@@ -3,23 +3,19 @@ package main
 import (
 	"log"
 
-	"frizz.io/tests/regenerate"
-
 	"os"
 
+	"frizz.io/tests/regenerate"
 	"github.com/dave/patsy/vos"
 )
 
 func main() {
 	env := vos.Os()
-
 	hashes, err := regenerate.Regenerate(env, os.Stdout, true)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	if err := regenerate.SaveHashes(env, hashes); err != nil {
 		log.Fatal(err)
 	}
-
 }
